6099a9cc1996bee39ac2ffd53b48109a4aa513b5
Petter Skerfving <pskerfving@me.com>
2012-05-08 07:50:38 +0200

    Sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,13 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Uefa::Application.initialize!
 
+
+ActionMailer::Base.smtp_settings = {
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain => "euro2012.ifkff.org",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
