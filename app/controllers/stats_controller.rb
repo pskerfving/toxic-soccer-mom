266daaefd01d00f0662f991fe20ced2b209f8965
Petter Skerfving <pskerfving@me.com>
2012-06-06 18:41:27 +0200

    Dagar kvar till första matchen.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -6,15 +6,17 @@ class StatsController < ApplicationController
     @nbr_tips = WinnersTip.all.length
     @nbr_wine = User.where(:wine => true).length
     @nbr_days_wine = time_until_wine_deadline
+    @nbr_days_register = time_until_first_game
   end
 
   def time_until_wine_deadline
     deadline = Time.utc(2012, "jun", 15)
     return ((deadline - Time.now)/(3600*24) + 1).round
   end
-  
+
   def time_until_first_game
     first_game = Game.order('kickoff').first
+    return ((first_game.kickoff - Time.now)/(3600*24) + 1).round
   end
 
 end
