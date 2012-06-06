351b1d86682375300e33deecd19bf5230ab528d6
Petter Skerfving <pskerfving@me.com>
2012-06-06 18:10:41 +0200

    Dagar kvar att lämna vin.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,9 +1,20 @@
+# coding: UTF-8
 class StatsController < ApplicationController
 
   def stats
     @nbr_users = User.all.length
     @nbr_tips = WinnersTip.all.length
     @nbr_wine = User.where(:wine => true).length
+    @nbr_days_wine = time_until_wine_deadline
+  end
+
+  def time_until_wine_deadline
+    deadline = Time.utc(2012, "jun", 15)
+    return ((deadline - Time.now)/(3600*24) + 1).round
+  end
+  
+  def time_until_first_game
+    first_game = Game.order('kickoff').first
   end
 
 end
