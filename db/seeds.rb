e65fe54ac914911a5deed58a8f5fe8219110cff2
Petter Skerfving <pskerfving@me.com>
2014-06-02 21:39:45 +0200

    glömt exakt vad...

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.create(name: 'Admin', email: 'pskerfving@me.com', admin: true)
