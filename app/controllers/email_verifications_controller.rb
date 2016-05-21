29c7dd3f0527ef963ae702bb11c29e1ef71ca545
pskerfving <pskerfving@me.com>
2016-05-21 15:56:32 +0200

    Baseline UEFA 2016

--- app/controllers/email_verifications_controller.rb
+++ app/controllers/email_verifications_controller.rb
@@ -5,7 +5,7 @@ class EmailVerificationsController < ApplicationController
     user.email_verified = true
     user.save!
     session[:user_id] = user.id
-#    redirect_to(:root, :notice => "Din mailadress är bekräftad") TODO: make this work in the test environment.
+    redirect_to(:root, :notice => "Vi gillar din mailadress!")
   end
 
 end
